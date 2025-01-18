8d32f9a246dd6a4bef4fef0208e68a45f7a4c5ca
0PrashantYadav0 <prashantyadav09783@gmail.com>
2025-01-18 21:27:13 +0530

    Chatbot fixed

--- src/lib/vectordb.ts
+++ src/lib/vectordb.ts
@@ -1,32 +1,32 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
 import { AstraDBVectorStore } from "@langchain/community/vectorstores/astradb";
-import { OpenAIEmbeddings } from "@langchain/openai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 
-const endpoint = process.env.ASTRA_DB_API_ENDPOINT || "";
-const token = process.env.ASTRA_DB_APPLICATION_TOKEN || "";
-const collection = process.env.ASTRA_DB_COLLECTION || "";
-
-if (!endpoint || !token || !collection) {
+if (!process.env.ASTRA_DB_API_ENDPOINT || !process.env.ASTRA_DB_APPLICATION_TOKEN || !process.env.ASTRA_DB_COLLECTION) {
   throw new Error("Please set environmental variables for Astra DB!");
 }
 
+const endpoint = process.env.ASTRA_DB_API_ENDPOINT;
+const token = process.env.ASTRA_DB_APPLICATION_TOKEN;
+const collection = process.env.ASTRA_DB_COLLECTION;
+
 export async function getVectorStore() {
+  const embeddings = new GoogleGenerativeAIEmbeddings({ 
+    model: "text-embedding-004" 
+  });
+
   return AstraDBVectorStore.fromExistingIndex(
-    new OpenAIEmbeddings({ model: "text-embedding-3-small" }),
+    embeddings,
     {
       token,
       endpoint,
       collection,
       collectionOptions: {
-        vector: { dimension: 1536, metric: "cosine" },
+        vector: { 
+          dimension: 768,  
+          metric: "cosine" 
+        },
       },
-    },
+    }
   );
-}
-
-export async function getEmbeddingsCollection() {
-  const client = new DataAPIClient(token);
-  const db = client.db(endpoint);
-
-  return db.collection(collection);
 }
